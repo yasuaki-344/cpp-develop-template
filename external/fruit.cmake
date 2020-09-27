bd16dff9e752190609de4550d46d4790bef63d48
yasuaki-miyoshi <ikusay.443@gmail.com>
2020-09-27 13:09:10 +0900

    Add google fruit
    This commit is to add google fruit library as external project.

---
+++ external/fruit.cmake
@@ -0,0 +1,23 @@
+include(ExternalProject)
+
+# directory setting
+set(FRUIT_BUILD_DIR ${CMAKE_BINARY_DIR}/fruit)
+set(FRUIT_INSTALL_DIR ${PROJECT_SOURCE_DIR}/external-libs/fruit)
+set(FRUIT_INCLUDE_DIR ${FRUIT_INSTALL_DIR}/include)
+set(FRUIT_LIB_DIR ${FRUIT_INSTALL_DIR}/lib)
+
+add_definitions(-DFRUIT_USES_BOOST=False)
+
+# external project setting
+ExternalProject_Add(
+    fruit
+    URL https://github.com/google/fruit/archive/v3.6.0.zip
+    PREFIX ${FRUIT_BUILD_DIR}
+    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=${FRUIT_INSTALL_DIR}
+               -DCMAKE_BUILD_TYPE=Release
+               -DFRUIT_USES_BOOST=False
+)
+
+
+# register include file path
+include_directories(${FRUIT_INCLUDE_DIR})
