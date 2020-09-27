08d055aa9c3b70d008d5b72f81e48d88e954a7cc
yasuaki-miyoshi <ikusay.443@gmail.com>
2020-09-27 10:09:06 +0900

    Add application project
    This commit is to add sample application project.

---
+++ src/app/src/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    static_cast<void>(argc);
+    static_cast<void>(argv);
+    std::cout << "appplication start" << std::endl;
+    return 0;
+}
