5377ddf718e0c0b5fc93786a0afa7ef63493c8e4
yasuaki-miyoshi <ikusay.443@gmail.com>
2020-09-27 10:16:19 +0900

    Modify application project
    Modify application project to enable application to call
    library function.

--- src/app/src/main.cc
+++ src/app/src/main.cc
@@ -1,9 +1,15 @@
+
 #include <iostream>
+
+#include "SampleClass.h"
 
 int main(int argc, char **argv)
 {
     static_cast<void>(argc);
     static_cast<void>(argv);
     std::cout << "appplication start" << std::endl;
+
+    auto lib = CppDevelopTemplate::lib::SampleClass();
+    lib.publicFunction();
     return 0;
 }
